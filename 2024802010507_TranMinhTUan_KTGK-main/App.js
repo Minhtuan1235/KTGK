17d09a590ce0e3c3561f5b594f3b2da1c89f25a6
Minhtuan1235 <145755494+Minhtuan1235@users.noreply.github.com>
2024-05-15 08:20:11 +0700

    Add files via upload

---
+++ 2024802010507_TranMinhTUan_KTGK-main/App.js
@@ -0,0 +1,28 @@
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import LoginScreen from './src/LoginScreen';
+import HomeScreen from './src/HomeScreen';
+import RegistrationScreen from './src/RegistrationScreen';
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+
+const Stack = createStackNavigator();
+
+export default function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+              <Stack.Screen name='Home' component={HomeScreen}/>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Registration" component={RegistrationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
